b465a3c1bc8a232d72ae0ff8dbade5955aab083d
samover <samuel.overloop@gmail.com>
2016-02-17 14:38:11 +0100

    fix typo and broken migration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true)  do
-      resourses :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy]
       resources :expenses, :only => [:show, :create, :update, :destroy]
       resources :categories, :only => [:index, :show, :create, :update, :destroy]
     end
--- db/migrate/20160217093525_add_user_ref_to_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToExpenses < ActiveRecord::Migration
-  def change
-    add_reference :expenses, :user, index: true, foreign_key: true
-  end
-end
