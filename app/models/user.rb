e9db0933c0629ffc3aec3602e75877d908674e90
samover <samuel.overloop@gmail.com>
2016-02-17 15:58:20 +0100

    creates auth_token upon user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_create -> { self.auth_token = SecureRandom.hex }
+
   has_many :expenses
   validates :email, presence: true, uniqueness: true, format: { :with => /@/ }
   validates :password, presence: true, confirmation: true
