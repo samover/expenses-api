d16568d8bbb9894ca12bc1f1711d1a54dba080c2
samover <samuel.overloop@gmail.com>
2016-02-17 08:50:25 +0100

    refactors expense controller

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::ExpensesController < ApplicationController
 
-  def index
-    expenses = Expense.all
-    render json: expenses
-  end
-
   def show
     expense = Expense.find(params[:id])
     render json: expense
