7cbf20f80c4886ce994afce262d12ef818a230b7
samover <samuel.overloop@gmail.com>
2016-02-21 11:47:36 +0100

    adds custom routes for sign_in and sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true)  do
-
-      resources :sessions, :only => [:create, :destroy]
+      #api_sessions POST   /sessions(.:format)       api/v1/sessions#create
+      #{:subdomain=>"api"}
+      post '/sessions(.:format)', to: 'sessions#create', as: 'sign_in'
+      delete '/sessions/:id(.:format)', to: 'sessions#destroy', as: 'sign_out'
+      #resources :sessions, :only => [:create, :destroy]
       resources :users, :only => [:show, :create, :update, :destroy]
       resources :expenses, :only => [:show, :create, :update, :destroy]
       resources :categories, :only => [:index, :show, :create, :update, :destroy]
